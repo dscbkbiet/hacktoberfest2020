5b896ff23af81453ff93ed76000266983ce7037e
Aniket Kumar Sinha <sinhaaniket32@gmail.com>
2020-10-10 14:46:41 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+
+import Hacker from './Hackers/Hacker';
+import Home from './Hackers/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        
+
+        <Switch>
+          <Route path="/hacker" component={Hacker} />
+          <Route path="/" component={Home} />
+        </Switch>
+
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/hacker">Hacker Name</Link>
+          </li>
+        </ul>
+      </nav>
+      </Router>
     </div>
   );
 }
---
+++ src/Hackers/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1><center>Hello Hacker</center></h1>
+        </div>
+    );
+}
