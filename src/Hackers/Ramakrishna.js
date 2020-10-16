36f3042264d25372cdc88a8cc5def48f529548b0
Ramakrishna Chhipa <54206397+RKC3000@users.noreply.github.com>
2020-10-16 18:35:22 +0530

    Ramakrishna file addded

--- src/Hackers/Ramakrishna.js
+++ src/Hackers/Ramakrishna.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Ramakrishna() {
     return (
-        <div >
+        <div>
             <h3><center>Hi, I am</center></h3>
             <h1><center>Ramakrishna Chhipa</center></h1>
         </div>
