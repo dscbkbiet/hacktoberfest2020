b10f4a8d80819435dbe0520bcfdb9d22719da987
Ramakrishna Chhipa <54206397+RKC3000@users.noreply.github.com>
2020-10-16 18:18:25 +0530

    added Ramakrishna file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 
 import Hacker from './Hackers/Hacker';
+import Ramakrishna from './Hackers/Ramakrishna';
 import Home from './Hackers/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        
+    return ( <
+        div className = "App" >
+        <
+        Router >
 
-        <Switch>
-          <Route path="/hacker" component={Hacker} />
-          <Route path="/" component={Home} />
-        </Switch>
 
-        <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+        <
+        Switch >
+        <
+        Route path = "/hacker"
+        component = { Hacker }
+        /> <
+        Route path = "/"
+        component = { Home }
+        /> < /
+        Switch >
 
-          <li>
-            <Link to="/hacker">Hacker Name</Link>
-          </li>
-        </ul>
-      </nav>
-      </Router>
-    </div>
-  );
+        <
+        nav >
+        <
+        ul >
+        <
+        li >
+        <
+        Link to = "/" > Home < /Link> < /
+        li >
+
+        <
+        li >
+        <
+        Link to = "/hacker" > Hacker Name < /Link> < /
+        li > <
+        li >
+        <
+        Link to = "/hacker" > Ramakrishna Chhipa < /Link> < /
+        li > <
+        /ul> < /
+        nav > <
+        /Router> < /
+        div >
+    );
 }
 
 export default App;
---
+++ src/Hackers/Ramakrishna.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Ramakrishna() {
+    return ( <
+        div >
+        <
+        h3 > < center > Hi, I am < /center></h
+        3 >
+        <
+        h1 > < center > Ramakrishna Chhipa < /center></h
+        1 >
+        <
+        /div>
+    );
+}
