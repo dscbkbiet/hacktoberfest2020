3c347b760b2e7439704c9f980962e6d00fba13eb
Anshika Gupta <64917165+AG9760@users.noreply.github.com>
2020-10-16 22:42:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 
 import Hacker from './Hackers/Hacker';
+import Anshika from './Hackers/Anshika';
 import Home from './Hackers/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
+    return ( 
+        <div className = "App" >
+        <Router >
         
-
+        
         <Switch>
-          <Route path="/hacker" component={Hacker} />
-          <Route path="/" component={Home} />
+        <Route path = "/hacker" component = { Hacker }/>
+        <Route path = "/" component = { Home }/> 
         </Switch>
 
-        <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+        <nav >
+        <ul >
+        <li >
+        <Link to = "/" > Home < /Link> 
+        </li>
+        <li >
+        <Link to = "/hacker" > Hacker Name < /Link> 
+        </li>
 
-          <li>
-            <Link to="/hacker">Hacker Name</Link>
-          </li>
-        </ul>
-      </nav>
-      </Router>
-    </div>
-  );
+        <li >
+        <Link to = "/hacker" > Anshika Gupta < /Link> 
+        </li> 
+        </ul> 
+        </nav> 
+        </Router> </div>
+    );
 }
 
 export default App;
