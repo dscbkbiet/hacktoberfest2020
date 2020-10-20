e911bb046481f44b807da331ccb4e8fbc6624a38
git-niharika <71392291+git-niharika@users.noreply.github.com>
2020-10-20 15:56:10 +0530

    Update Niharika.js
    unable to do it from git bash. thus did it directly

--- src/Hackers/Niharika.js
+++ src/Hackers/Niharika.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function Aniket() {
+export default function Niharika() {
     return (
         <div>
             <h3><center>Hi, I am</center></h3>
-            <h1><center>Aniket</center></h1>
+            <h1><center>Niharika</center></h1>
+            <h1><center>You are braver than you believe, stronger than you seem and smarter than you seem.</center></h1>
         </div>
     );
 }
